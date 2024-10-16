334e7d99b1943f9cbe0becf8c062885fab68a797
Eloah0407 <00001095124158sp@al.educacao.sp.gov.br>
2024-10-16 20:19:21 -0300

    adicionando css html e js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, livre, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento,14, drama
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+   let recomendacao = "A viagem de Chihiro";
+   text(recomendacao, width/2, height/2);
+  
+  }
